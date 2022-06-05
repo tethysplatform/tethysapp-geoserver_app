4cf737a382e26292422ac5512fe85021eb7f040a
Nathan Swain <nswain@aquaveo.com>
2022-06-04 21:41:56 -0600

    Updates for Tethys 4

--- tethysapp/geoserver_app/controllers.py
+++ tethysapp/geoserver_app/controllers.py
@@ -48,3 +48,71 @@ def home(request):
     context = {}
 
     return render(request, 'geoserver_app/home.html', context)
+
+
+@controller
+def map(request):
+    """
+    Controller for the map page
+    """
+    geoserver_engine = app.get_spatial_dataset_service(name='main_geoserver', as_engine=True)
+
+    options = []
+
+    response = geoserver_engine.list_layers(with_properties=False)
+
+    if response['success']:
+        for layer in response['result']:
+            options.append((layer.title(), layer))
+
+    select_options = SelectInput(
+        display_text='Choose Layer',
+        name='layer',
+        multiple=False,
+        options=options
+    )
+
+    map_layers = []
+
+    if request.POST and 'layer' in request.POST:
+        selected_layer = request.POST['layer']
+        legend_title = selected_layer.title()
+
+        geoserver_layer = MVLayer(
+            source='ImageWMS',
+            options={
+                'url': 'http://localhost:8181/geoserver/wms',
+                'params': {'LAYERS': selected_layer},
+                'serverType': 'geoserver'
+            },
+            legend_title=legend_title,
+            legend_extent=[-114, 36.5, -109, 42.5],
+            legend_classes=[
+                MVLegendClass('polygon', 'County', fill='#999999'),
+        ])
+
+        map_layers.append(geoserver_layer)
+
+
+    view_options = MVView(
+        projection='EPSG:4326',
+        center=[-100, 40],
+        zoom=4,
+        maxZoom=18,
+        minZoom=2
+    )
+
+    map_options = MapView(
+        height='500px',
+        width='100%',
+        layers=map_layers,
+        legend=True,
+        view=view_options
+    )
+
+    context = {
+        'map_options': map_options,
+        'select_options': select_options
+    }
+
+    return render(request, 'geoserver_app/map.html', context)
